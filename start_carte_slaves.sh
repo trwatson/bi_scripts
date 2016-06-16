b24b1bc748872e5cb8f10180acc30a583182fe30
trwatson <troy@iwatson.net>
2016-06-16 16:14:39 -0600

    generalized

--- start_carte_slaves.sh
+++ start_carte_slaves.sh
@@ -12,11 +12,7 @@ cd /pentaho/pentaho/design-tools/data-integration
 #normal slaves
 for (( slave=1; slave<=$NORMAL_SLAVES; slave++ ))
 do
-        ./carte.sh carte-slave0$slave-config.xml > /pentaho/pentaho/server/data-integration-server/logs/slaves/slave0$slave.log 2>&1 &
+        ./carte.sh carte-slave0$slave-config.xml > logs/slave0$slave.log 2>&1 &
 done
-#async slave
-./carte.sh carte-slave-async-config.xml > /pentaho/pentaho/server/data-integration-server/logs/slaves/slave-async.log 2>&1 &
-#sequence slave
-./carte.sh carte-slave-sequence-config.xml > /pentaho/pentaho/server/data-integration-server/logs/slaves/slave-sequence.log 2>&1 &
 
 exit 0
